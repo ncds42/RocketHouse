f6afb33e200ce19395287102a1d0ee3f4a9b4768
nitish chauhan <ncds42@gmail.com>
2019-08-14 07:55:56 -0500

    fixed gallery for high res

--- routes/views/gallery.js
+++ routes/views/gallery.js
@@ -1,25 +1,67 @@
-var keystone = require('keystone');
+var keystone = require("keystone");
 
-
-exports = module.exports = function (req, res) {
-	
+exports = module.exports = function(req, res) {
 	// DEBUG STATEMENT
 	// console.log(__filename.split("vs-workspace")[1] + '... was executed!')
-	
+
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
+	locals.range = function(a, b, step) {
+		var range = [];
+
+		if (typeof a == "number") {
+			range[0] = a;
+			step = step || 1;
+			while (a + step <= b) {
+				range[range.length] = a += step;
+			}
+		} else {
+			var s = "abcdefghijklmnopqrstuvwxyz";
+
+			if (a === a.toUpperCase()) {
+				b = b.toUpperCase();
+				s = s.toUpperCase();
+			}
+
+			s = s.substring(s.indexOf(a), s.indexOf(b) + 1);
+			range = s.split("");
+		}
+
+		return range;
+	};
+
 	// Set locals
-	locals.section = 'gallery';
+	locals.section = "gallery";
 
 	// Load the galleries by sortOrder
-	view.query('galleries', keystone.list('Gallery').model.find())
-		// .model
-		// .find()
-		// .sort('sortOrder'));
+	view.query("galleries", keystone.list("Gallery").model.find());
+	// .sort('sortOrder'));
 
+	exports.range = function(a, b, step) {
+		var range = [];
+
+		if (typeof a == "number") {
+			range[0] = a;
+			step = step || 1;
+			while (a + step <= b) {
+				range[range.length] = a += step;
+			}
+		} else {
+			var s = "abcdefghijklmnopqrstuvwxyz";
+
+			if (a === a.toUpperCase()) {
+				b = b.toUpperCase();
+				s = s.toUpperCase();
+			}
+
+			s = s.substring(s.indexOf(a), s.indexOf(b) + 1);
+			range = s.split("");
+		}
+
+		return range;
+	};
 
 	// Render the view
-	view.render('gallery');
-
+	view.render("gallery");
 };
